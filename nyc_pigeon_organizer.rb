2b925bb188da391adbef2f3eff8639dd98a55401
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-30 02:00:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,12 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   hash = { }
 
-  data
+  data[:gender][:male].each do |bird|
+    hash[bird] = {
+      :color
+      :gender
+      :lives 
+    }
 
 end
 pp hash
