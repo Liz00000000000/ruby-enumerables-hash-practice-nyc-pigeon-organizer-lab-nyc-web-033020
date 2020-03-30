06b5081f263d8fac97ff9d0cf3042d271347ed52
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-30 02:30:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,14 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   hash = { }
 
+data.each do |colors_gender_lives, values|
+  values.each do |value, array|
+    array.each do |name|
+      if hash[name] == nil
+        hahs[name] = { }
+    end 
+    end 
+
+
   
 end
